ac3286b38a65cd6258d591a34dc535db7b5d5fd3
Megan Harrison <mtharrison@alumni.fullsail.edu>
2023-09-17 18:39:44 -0500

    start of CRUD

--- server/app.py
+++ server/app.py
@@ -1,19 +1,30 @@
 # Remote library imports
-from flask import request, make_response, session
+from flask import request, make_response, session, jsonify
 from flask_restful import Resource
 import ipdb
 from flask_migrate import Migrate
 
 # Local imports
-from config import app, api
+from config import app, api, db
 # Add your model imports
 from models import User, Prediction, Game, Comment
 
-#should the secret key go here?
-
 @app.route('/')
 def hello():
-    return 'Hello from Flask Backend!'
+    return '<h1>Capstone Project</h1>'
+
+class Users(Resource):
+    def get(self):
+        q = [user.to_dict(rules = ()) for user in User.query.all()]
+        response = make_response(q, 200)
+        return response
+    
+    def post(self):
+        data = request.get_json()
+        new_user = User(username=data['username'], password=data['password'])
+        db.session.add(new_user)
+        db.session.commit()
+        return make_response(new_user.to_dict(), 200)
 
 
 if __name__ == '__main__':
